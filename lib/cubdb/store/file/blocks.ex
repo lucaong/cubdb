938e43e74c0bc75deaf11da3dec07aec65a47c42
Luca Ongaro <lukeongaro@gmail.com>
2017-06-04 12:40:12 +0200

    refactor and DRY Blocks

--- lib/cubdb/store/file/blocks.ex
+++ lib/cubdb/store/file/blocks.ex
@@ -2,15 +2,33 @@ defmodule CubDB.Store.File.Blocks do
   @block_size 4096
 
   def add_headers(bin, loc, block_size \\ @block_size) do
+    at_block_boundary(bin, loc, block_size, &add/3)
+  end
+
+  def strip_headers(bin, loc, block_size \\ @block_size) do
+    at_block_boundary(bin, loc, block_size, &strip/3)
+  end
+
+  def length_with_headers(loc, length, block_size \\ @block_size) do
     case rem(loc, block_size) do
-      0 -> add(bin, <<>>, block_size)
+      0 -> headers_length(length, block_size) + length
+      r ->
+        prefix = block_size - r
+        rest = length - prefix
+        prefix + headers_length(rest, block_size) + rest
+    end
+  end
+
+  defp at_block_boundary(bin, loc, block_size, function) do
+    case rem(loc, block_size) do
+      0 -> function.(bin, <<>>, block_size)
       r ->
         block_rest = block_size - r
         if byte_size(bin) <= block_rest do
           bin
         else
           <<prefix::binary-size(block_rest), rest::binary>> = bin
-          add(rest, prefix, block_size)
+          function.(rest, prefix, block_size)
         end
     end
   end
@@ -25,20 +43,6 @@ defmodule CubDB.Store.File.Blocks do
     end
   end
 
-  def strip_headers(bin, loc, block_size \\ @block_size) do
-    case rem(loc, block_size) do
-      0 -> strip(bin, <<>>, block_size)
-      r ->
-        block_rest = block_size - r
-        if byte_size(bin) <= block_rest do
-          bin
-        else
-          <<prefix::binary-size(block_rest), rest::binary>> = bin
-          strip(rest, prefix, block_size)
-        end
-    end
-  end
-
   defp strip(bin, acc, block_size) do
     if byte_size(bin) <= block_size do
       <<_::binary-1, block::binary>> = bin
@@ -47,16 +51,6 @@ defmodule CubDB.Store.File.Blocks do
       data_size = block_size - 1
       <<_::binary-1, block::binary-size(data_size), rest::binary>> = bin
       strip(rest, acc <> block, block_size)
-    end
-  end
-
-  def length_with_headers(loc, length, block_size \\ @block_size) do
-    case rem(loc, block_size) do
-      0 -> headers_length(length, block_size) + length
-      r ->
-        prefix = block_size - r
-        rest = length - prefix
-        prefix + headers_length(rest, block_size) + rest
     end
   end
 
