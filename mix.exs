4455996ff735c2f3a0d4f6d3466932cd9fe4698e
Luca Ongaro <lukeongaro@gmail.com>
2017-06-05 18:54:32 +0200

    consistent formatting

--- mix.exs
+++ mix.exs
@@ -33,7 +33,7 @@ defmodule CubDB.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
-      { :quixir, "~> 0.9", only: :test },
+      {:quixir, "~> 0.9", only: :test},
       {:dialyxir, "~> 0.5", only: [:dev], runtime: false}
     ]
   end
