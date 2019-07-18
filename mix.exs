8c3dd15b24836f3439ac1baba0038760c7363243
Luca Ongaro <mail@lucaongaro.eu>
2019-07-18 19:05:12 +0200

    Travis fix

--- mix.exs
+++ mix.exs
@@ -17,7 +17,7 @@ defmodule CubDB.Mixfile do
       ],
       dialyzer: [ignore_warnings: "dialyzer_ignore.exs"],
       test_coverage: [tool: ExCoveralls],
-      preferred_cli_env: [coveralls: :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test]
+      preferred_cli_env: [coveralls: :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test, "coveralls.travis": :test]
     ]
   end
 
