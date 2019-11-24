0dbc9bb1c201a65982c116ec2bf83cbcc835f8e0
Luca Ongaro <mail@lucaongaro.eu>
2019-11-24 19:26:29 +0100

    Version v0.13.1
    Changes:
    
      - [bugfix] fix size calculation in presence of delete markers

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule CubDB.Mixfile do
   def project do
     [
       app: :cubdb,
-      version: "0.13.0",
+      version: "0.13.1",
       elixir: "~> 1.6",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
