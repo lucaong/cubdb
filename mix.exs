375065fb4b1dc4f6f377e8d5351d5588e9fa68ea
Luca Ongaro <mail@lucaongaro.eu>
2020-01-16 17:16:47 +0100

    Version v0.14.0
    Changes:
    
      - Better child_spec/1 function, allowing to pass both data_dir and
        options when starting CubDB as part of a supervision tree.

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule CubDB.Mixfile do
   def project do
     [
       app: :cubdb,
-      version: "0.13.1",
+      version: "0.14.0",
       elixir: "~> 1.6",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
