93e29a105e7d214ca456281f7ed7891c0b5b6e5e
Luca Ongaro <mail@lucaongaro.eu>
2020-04-12 21:21:03 +0200

    Version 1.0.0-rc.1
    Changes:
    
      - [breaking change] better defaults for `CubDB`:
        * `auto_file_sync` now defaults to `true` (slower but durable)
        * `auto_compact` now defaults to `true`
      - [breaking change] `select/2` now takes the timeout as an option
        instead of an additional argument
      - [breaking change] `get_and_update_multi/4` now takes the timeout as
        an option instead of an additional argument
      - Better internal handling of timeouts that ensures cleanup of
        resources on the callee side

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule CubDB.Mixfile do
   def project do
     [
       app: :cubdb,
-      version: "0.17.0",
+      version: "1.0.0-rc.1",
       elixir: "~> 1.6",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
