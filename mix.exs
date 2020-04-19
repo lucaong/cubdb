61da0fdef0f4ecb953328850e988acdfcb77262b
Luca Ongaro <mail@lucaongaro.eu>
2020-04-19 23:50:45 +0200

    Version v1.0.0-rc.2
    Changes:
    
      - `CubDB.put_and_delete_multi/3` to atomically put and delete entries
      - `CubDB.put_new/3` to put an entry only if the key does not exist yet
      - More efficient implementation of `CubDB.put_multi/2` and
        `CubDB.delete_multi/2`
      - `CubDB.get_multi/2` does not block writers

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule CubDB.Mixfile do
   def project do
     [
       app: :cubdb,
-      version: "1.0.0-rc.1",
+      version: "1.0.0-rc.2",
       elixir: "~> 1.6",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
