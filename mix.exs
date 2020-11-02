04cdc513739ccb19e959616f7c56e0d814bf68e9
Luca Ongaro <mail@lucaongaro.eu>
2020-11-02 19:54:31 +0100

    fix coverage report

--- mix.exs
+++ mix.exs
@@ -46,7 +46,7 @@ defmodule CubDB.Mixfile do
       {:dialyxir, "~> 1.0.0", only: [:dev], runtime: false},
       {:ex_doc, "~> 0.21", only: :dev, runtime: false},
       {:benchee, "~> 1.0", only: :dev},
-      {:excoveralls, "~> 0.12", only: :test}
+      {:excoveralls, "~> 0.13.3", only: :test}
     ]
   end
 
